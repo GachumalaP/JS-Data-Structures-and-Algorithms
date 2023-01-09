a9e979b1e4728030e9963ceacad8e2d48d072c15
gachumalapriyanka <priyankagachumala@gmail.com>
2023-01-09 14:37:42 -0500

    level order traversal

---
+++ data-structures/tree/tree-traversal/levelorder-traversal.js
@@ -0,0 +1,18 @@
+//Input: root = [3,9,20,null,null,15,7]
+//Output: [3,9,20,15,7]
+//Leetcode https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+
+
+const levelOrderTraversal = (node) => {
+    if (node == null) return [];
+
+    let result = [];
+    let currentLevelNodes = [node];
+    while (currentLevelNodes.length != 0) {
+        let currentNode = currentLevelNodes.shift();
+        result.push(currentNode.val);
+        if (currentNode.left) currentLevelNodes.push(currentNode.left);
+        if (currentNode.right) currentLevelNodes.push(currentNode.right);
+    }
+    return result;
+}
