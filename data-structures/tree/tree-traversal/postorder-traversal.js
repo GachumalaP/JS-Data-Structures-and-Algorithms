fe192a9aaf8621e0d8cac475f4d4b180c87af561
gachumalapriyanka <priyankagachumala@gmail.com>
2023-01-09 14:41:06 -0500

    postorder traversal

--- data-structures/tree/tree-traversal/postorder-traversal.js
+++ data-structures/tree/tree-traversal/postorder-traversal.js
@@ -1,4 +1,5 @@
-//  left > right > root
+// left > right > root
+// leet code https://leetcode.com/problems/binary-tree-postorder-traversal/
 
 function TreeNode(val, left, right) {
     this.val = (val === undefined ? 0 : val)
@@ -8,6 +9,7 @@ function TreeNode(val, left, right) {
 
 // @param {TreeNode} root
 
+// recursive way
 const postorderTraversal = (root) => {
 
     //check if root is null
